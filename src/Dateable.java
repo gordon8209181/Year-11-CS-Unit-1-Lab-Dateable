795f7fc611c77c6401b5dd97e95b7315c844a4d1
gordon8209181 <gordonz2027@student.cis.edu.hk>
2025-02-21 14:15:40 +0800

    Update Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,7 +1,13 @@
 public class Dateable {
 
   public static void main(String[] args) {
-      // Write your code below, inside the main method.
+      // Write your code below, inside the main method
+    Scanner scanner= new Scanner(System.in);
+    System.out.println("Please enter your age");
+    int age= scanner.nextInt();
+    int num1= age/2;
+    int least= num1+7;
+    System.out.println(age+"years old should date someone who is at least"+least+"years old") ;
     
   }
   
